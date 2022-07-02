74598eb011ae30256299e2e76d19b932399d6631
serhiibeznisko <beznisko.ss@gmail.com>
2022-07-02 13:33:49 +0200

    Add connection hook and channel page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
 
 import App from './components/app';
